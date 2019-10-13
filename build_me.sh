4447cbc70b55d65dada47a24d6e63e90fb523a27
George Tabacar <g.tabacar@purplesrl.com>
2019-10-14 00:13:27 +0300

    initial iso build

--- build_me.sh
+++ build_me.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker build -t bootr:latest src
+docker run --rm --name bootr -ti bootr:latest /bin/sh
---
+++ src/mkimg.bootr.sh
@@ -0,0 +1,19 @@
+profile_bootr() {
+        profile_standard
+        kernel_cmdline="console=tty0 console=ttyS0,115200"
+        syslinux_serial="0 115200"
+        kernel_addons="zfs spl"
+        apks="$apks iscsi-scst zfs-scripts zfs zfs-utils-py
+                cciss_vol_status lvm2 mdadm mkinitfs mtools nfs-utils
+                parted rsync sfdisk syslinux unrar util-linux xfsprogs
+                dosfstools ntfs-3g
+                "
+        local _k _a
+        for _k in $kernel_flavors; do
+                apks="$apks linux-$_k"
+                for _a in $kernel_addons; do
+                        apks="$apks $_a-$_k"
+                done
+        done
+        apks="$apks linux-firmware"
+}
