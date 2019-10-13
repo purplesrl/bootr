8d22a3cd0fa47f2ed33a4c08c54cc05b6988669b
George Tabacar <g.tabacar@purplesrl.com>
2019-10-14 01:04:36 +0300

    Added overlay

--- src/mkimg.bootr.sh
+++ src/mkimg.bootr.sh
@@ -3,10 +3,11 @@ profile_bootr() {
         kernel_cmdline="console=tty0 console=ttyS0,115200"
         syslinux_serial="0 115200"
         kernel_addons="zfs spl"
-        apks="$apks iscsi-scst zfs-scripts zfs zfs-utils-py
+        apks="$apks iscsi-scst zfs-scripts zfs zfs-utils-py 
                 cciss_vol_status lvm2 mdadm mkinitfs mtools nfs-utils
                 parted rsync sfdisk syslinux unrar util-linux xfsprogs
                 dosfstools ntfs-3g
+		nano openvpn
                 "
         local _k _a
         for _k in $kernel_flavors; do
@@ -16,4 +17,5 @@ profile_bootr() {
                 done
         done
         apks="$apks linux-firmware"
+        apkovl="genapkovl-bootr.sh"
 }
